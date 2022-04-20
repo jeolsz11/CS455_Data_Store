0c878275166465252789c0a5ccd3206985d9085d
chall0616 <103540214+chall0616@users.noreply.github.com>
2022-04-20 16:51:44 -0500

    Update Engine_to_Data_Store.py

--- Engine_to_Data_Store.py
+++ Engine_to_Data_Store.py
@@ -24,17 +24,19 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
             conn.sendall(json_data) 
 
 # Connect to MariaDB Platform
-#try:
-#	conn = MySQLdb.connect(
-#	host="cs.csis.work",
-#	port=22,
-#	user="monstore",
-#	password="455-mon-store",
-#	autocommit=True
-#	)
-#except MySQLdb.Error as e:
-#	print(f"Error connecting to MariaDB Platform: {e}")
-#	sys.exit(1)
+"""
+try:
+	conn = MySQLdb.connect(
+	host="cs.csis.work",
+	port=22,
+	user="monstore",
+	password="455-mon-store",
+	autocommit=True
+	)
+except MySQLdb.Error as e:
+	print(f"Error connecting to MariaDB Platform: {e}")
+	sys.exit(1)
+"""
 
 # try to connect to the database
 try:
@@ -43,7 +45,8 @@ except:
     print("Can't connect to database")
 else:
     #successful connection
-    print("Connected to database") 
+    print("Connected to database")
+    print()
 
 # Instantiate Cursor
 cursor = db.cursor()
@@ -57,19 +60,22 @@ for i in cursor:
     data = cursor.fetchone()
     if data:
         print(data)
+        print()
     else:
         print("Error") 
 
 # add data
-#try:
-#	cursor.execute("INSERT INTO devices() VALUES('" + json_data + "')")
-#except:
-#    print("Failed to insert values") 
-
+"""
+try:
+	cursor.execute("INSERT INTO devices() VALUES('" + json_data + "')")
+except:
+    print("Failed to insert values") 
+"""
 
 # try to insert new values into the devices table
 json_data1 = { "ID": "4486d8dc-9258-45e1-8a41-816bcd6f5ea3", "Time Stamp": "22:03:29", "CPU": 8, "DISK": 44, "MEMORY": 31, "NETWORK": 8 }
 json_data2 = { "ID": "4486d8dc-9258-45e1-8a41-816bcd6f5ea1", "Time Stamp": "22:01:29", "CPU": 1, "DISK": 40, "MEMORY": 30, "NETWORK": 2 }
+json_data3 = { "ID": "00ce9834-8453-4580-8082-4d2748c84e65", "Time Stamp": 1650469613, "CPU": 10, "DISK": 3, "MEMORY": 66, "NETWORK": { "BYTES SENT": 1554241536, "BYTES RECIEVED": 3376441344 } }
 query = 'INSERT INTO devices() VALUES(%s)'
 
 try:
@@ -80,6 +86,7 @@ else:
     db.commit()
     # print that 1 row was inserted if successful  
     print(cursor.rowcount, "record inserted.") 
+    print()
 
 # print table with the new row that was inserted 
 cursor.execute('SELECT * FROM devices')
@@ -87,22 +94,25 @@ for i in cursor:
     data = cursor.fetchone()
     if data:
         print(data)
+        print()
     else:
         print("Error") 
 
 # update data
-#	cursor.execute('''
-#    SELECT 
-#		JSON_REPLACE(metrics, '$.Time Stamp', 'new_time'), 
-#		JSON_REPLACE(metrics, '$.CPU', 'new_cpu'),
-#		JSON_REPLACE(metrics, '$.DISK', 'new_disk'),
-#		JSON_REPLACE(metrics, '$.MEMORY', 'new_memory'),
-#		JSON_REPLACE(metrics, '$.NETWORK', 'new_network'),
-#		FROM devices WHERE JSON_VALUE(metrics, '$.ID') = '4486d8dc-9258-45e1-8a41-816bcd6f5ea3'
-#        ''')
+"""
+	cursor.execute('''
+    SELECT 
+		JSON_REPLACE(metrics, '$.Time Stamp', 'new_time'), 
+		JSON_REPLACE(metrics, '$.CPU', 'new_cpu'),
+		JSON_REPLACE(metrics, '$.DISK', 'new_disk'),
+		JSON_REPLACE(metrics, '$.MEMORY', 'new_memory'),
+		JSON_REPLACE(metrics, '$.NETWORK', 'new_network'),
+		FROM devices WHERE JSON_VALUE(metrics, '$.ID') = '4486d8dc-9258-45e1-8a41-816bcd6f5ea3'
+        ''')
 
-#db.commit()
-	
+db.commit()
+"""
+
 # close connection
 db.close()
 
