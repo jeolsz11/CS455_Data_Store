077d995dfe991e40732dfea924b16ccc0cb70b39
chall0616 <103540214+chall0616@users.noreply.github.com>
2022-04-25 20:43:49 -0500

    Update Engine_to_Data_Store.py

--- Scripts/Engine_to_Data_Store.py
+++ Scripts/Engine_to_Data_Store.py
@@ -4,42 +4,90 @@
 
 # Module Imports
 import MySQLdb
-#import sys
+import sys
 import json
 import socket
 
+def insert_into_database(db, json_data):
+    # Instantiate Cursor
+    cursor = db.cursor()
+
+    # Testing SELECT so show everything in the devices table
+    cursor.execute('SELECT * FROM devices')
+    for i in cursor:
+        data = cursor.fetchone()
+        if data:
+            #print(i)
+            #print()
+            print(data)
+            print()
+        else:
+            print("Error") 
+
+    query = 'INSERT INTO devices() VALUES(%s)'
+    try:
+	    cursor.execute(query, (json.dumps(json_data),))
+    except:
+        print("Failed to insert values")
+    else:
+        db.commit()
+        # Print that 1 row was inserted if successful  
+        print(cursor.rowcount, "record inserted.") 
+        print()
+
+    # Print table with the new row that was inserted 
+    cursor.execute('SELECT * FROM devices')
+    for i in cursor:
+        data = cursor.fetchone()
+        if data:
+            #print(i)
+            #print()
+            print(data)
+            print()
+        else:
+            print("Error")
+
+
 HOST = "localhost"
-PORT = # port to listen on 
+PORT = 8081
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as soc:
-    soc.bind((HOST, PORT))
-    soc.listen()
-    conn, addr = soc.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            json_data = conn.recv(1024)
-            if not json_data:
-                break
-            conn.sendall(json_data)
-            #conn.sendall.encode("UTF-8") 
+    soc.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    try:
+        soc.bind((HOST, PORT))
+    except:
+        print("Bind failed.")
+        sys.exit()
+    else:
+        print("Socket Binded.") 
+        soc.listen()
+        print("Listening...")
+        conn, addr = soc.accept()
+        conn.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        with conn:
+            print(f"Connected by {addr}")
+            while True:
+                json_data = conn.recv(1024).decode()
+                print("Received: ", json_data) 
+                if not json_data:
+                    break                
+                try:
+                    db = MySQLdb.connect("localhost","monstore","455-mon-store","monstore")
+                except:
+                    print("Can't connect to database")
+                else:
+                    #successful connection
+                    print("Connected to database")
+                    print()
+                    insert_into_database(db, json_data)
+		            # Close connection
+                    db.close()
+                #conn.sendall(json_data)
+                #conn.sendall.encode("UTF-8") 
 
-# Connect to MariaDB Platform
-"""
-try:
-	conn = MySQLdb.connect(
-	host="cs.csis.work",
-	port=22,
-	user="monstore",
-	password="455-mon-store",
-	autocommit=True
-	)
-except MySQLdb.Error as e:
-	print(f"Error connecting to MariaDB Platform: {e}")
-	sys.exit(1)
-"""
 
-# try to connect to the database
+# Try to connect to the database
+"""
 try:
     db = MySQLdb.connect("localhost","monstore","455-mon-store","monstore")
 except:
@@ -48,32 +96,28 @@ else:
     #successful connection
     print("Connected to database")
     print()
-
+"""
+"""
 # Instantiate Cursor
 cursor = db.cursor()
 	
-# hard coded JSON for testing in isolation
+# Hard coded JSON for testing in isolation
 #json_data = '{ "ID": "4486d8dc-9258-45e1-8a41-816bcd6f5ea3", "Time Stamp": "22:03:29", "CPU": 8, "DISK": 44, "MEMORY": 31, "NETWORK": 8 }'
 
-# testing SELECT so show everything in the devices table
+# Testing SELECT so show everything in the devices table
 cursor.execute('SELECT * FROM devices')
 for i in cursor:
     data = cursor.fetchone()
     if data:
+        #print(i)
+        #print()
         print(data)
         print()
     else:
         print("Error") 
 
-# add data
-"""
-try:
-	cursor.execute("INSERT INTO devices() VALUES('" + json_data + "')")
-except:
-    print("Failed to insert values") 
-"""
-
-# try to insert new values into the devices table
+# Add data
+# Try to insert new values into the devices table
 json_data1 = { "ID": "4486d8dc-9258-45e1-8a41-816bcd6f5ea3", "Time Stamp": "22:03:29", "CPU": 8, "DISK": 44, "MEMORY": 31, "NETWORK": 8 }
 json_data2 = { "ID": "4486d8dc-9258-45e1-8a41-816bcd6f5ea1", "Time Stamp": "22:01:29", "CPU": 1, "DISK": 40, "MEMORY": 30, "NETWORK": 2 }
 json_data3 = { "ID": "00ce9834-8453-4580-8082-4d2748c84e65", "Time Stamp": 1650469613, "CPU": 10, "DISK": 3, "MEMORY": 66, "NETWORK": { "BYTES SENT": 1554241536, "BYTES RECIEVED": 3376441344 } }
@@ -85,38 +129,26 @@ except:
     print("Failed to insert values")
 else:
     db.commit()
-    # print that 1 row was inserted if successful  
+    # Print that 1 row was inserted if successful  
     print(cursor.rowcount, "record inserted.") 
     print()
 
-# print table with the new row that was inserted 
+# Print table with the new row that was inserted 
 cursor.execute('SELECT * FROM devices')
 for i in cursor:
     data = cursor.fetchone()
     if data:
+        #print(i)
+        #print()
         print(data)
         print()
     else:
         print("Error") 
-
-# update data
-"""
-	cursor.execute('''
-    SELECT 
-		JSON_REPLACE(metrics, '$.Time Stamp', 'new_time'), 
-		JSON_REPLACE(metrics, '$.CPU', 'new_cpu'),
-		JSON_REPLACE(metrics, '$.DISK', 'new_disk'),
-		JSON_REPLACE(metrics, '$.MEMORY', 'new_memory'),
-		JSON_REPLACE(metrics, '$.NETWORK', 'new_network'),
-		FROM devices WHERE JSON_VALUE(metrics, '$.ID') = '4486d8dc-9258-45e1-8a41-816bcd6f5ea3'
-        ''')
-
-db.commit()
 """
 
-# close sockets
-conn.close()
-soc.close()
-# close connection
-db.close()
+# Close sockets
+#conn.close()
+#soc.close()
+# Close connection
+#db.close()
 
