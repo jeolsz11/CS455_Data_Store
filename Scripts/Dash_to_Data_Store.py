5a3a11d1c9cd0ebbfa11f392723d6556a32508af
jeolsz11 <103540507+jeolsz11@users.noreply.github.com>
2022-04-25 20:36:48 -0500

    Update Dash_to_Data_Store.py
    Begin changes to using websockets library

--- Scripts/Dash_to_Data_Store.py
+++ Scripts/Dash_to_Data_Store.py
@@ -4,78 +4,18 @@
 # Data Store acts as the server due to being hosted on the CS server
 # Dash acts as the client due to being external to the CS server
 
+# using websockets to communicate with dash due to dash being browser based
+
 # Module Imports
-import sys
-import MySQLdb
-import json
-import socket
+import asyncio
+import websockets
 
-# specify Host and Port 
-HOST = 'localhost' 
-PORT = 8082
+async def echo(websocket):
+    async for message in websocket:
+        await websocket.send(message)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+async def main():
+    async with websockets.serve(echo, 'cs.csis.work', 8082):
+        await asyncio.Future()  # run forever
 
-# binding host and port
-try:
-    s.bind((HOST, PORT))    
-except socket.error as message:
-    print('>> Bind failed.')
-    sys.exit() 
-else: 
-    print('>> Socket binded')
-   
-# starts listening
-s.listen()
-print ('>> Listening...')
-   
-# receive from Dash   
-#while True:
-connect, addr = s.accept() # establish connection with Dash
-print ('>> Connected')
-query = connect.recv(1024).decode('utf-8')
-print ('>> Received ', repr(query))
-
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-# Connect to MariaDB Platform
-#try: 
-#   connect = MySQLdb.connect(
-#	host="localhost",
-#	user="monstore",
-#	password="455-mon-store",
-#	database="monstore",
-#	autocommit=True)
-#except MySQLdb.Error as e:
-#	print(f"Error connecting to database: {e}")
-#	sys.exit(1)
-#else:
-#	print("Connected to database")
-
-# Instantiate Cursor
-#cursor = connect.cursor()
-
-# get data from database; currently there is only one query type
-#query = "SELECT * FROM devices"
-#cursor.execute(query)
-
-# write results to metrics.json; writing over what was previously there
-#json_object = json.dumps(dictionary, indent = 4)
-#with open("metrics.json", "w") as outfile:
-#outfile.write(json_object)
-# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-
-filename = 'metrics.json' # file MUST be in same folder or path as program
-file = open(filename,'rb')
-l = file.read(1024)
-
-#while (l):
-connect.sendall('Data Store'.encode('utf-8'))
-print('>> Sent ', repr(l))
-l = file.read(1024)
-file.close()
-
-print('>> Done sending')
-
-# close socket and connection		
-s.close()
-connect.close()
+asyncio.run(main())
